f854ee6ca2fbadb454a9cf750545194088026e30
XiaoXinAir14 <15252202305@163.com>
2022-05-21 15:17:46 +0800

    scannertest

--- src/gitTest/XiaoxinAir14.java
+++ src/gitTest/XiaoxinAir14.java
@@ -1,7 +1,11 @@
 package gitTest;
 
+import java.util.Scanner;
+
 public class XiaoxinAir14 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("HEllo xiaoxin");
+
     }
 }
